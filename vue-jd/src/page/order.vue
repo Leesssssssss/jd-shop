6eb786a88a7267589ebf88c27fd65675006d42fc
Leesssssssss <772592971@qq.com>
2018-10-29 21:41:14 +0800

    order

--- vue-jd/src/page/order.vue
+++ vue-jd/src/page/order.vue
@@ -20,6 +20,73 @@
       <img src="../../static/img/backRight.png" alt="">
     </div>
 
+    <!-- 商品信息 -->
+    <div class="cart">
+      <div class="shopName">
+        <img src="../../static/img/jdd.png" alt="">
+        <span class="shopNameTitle">京东自营</span>
+      </div>
+      <div class="goodList" v-for="(good,index) in goods">
+        <img :src="good.pic" alt="">
+        <div class="goodInfo">
+          <div class="goodInfoTitle">
+            <img src="https://img11.360buyimg.com/jdphoto/s102x28_jfs/t14512/288/2659278877/2368/8468a10d/5aadf9daNd4909ddc.png!q70.webp" alt="">
+            <span class="goodInfoText">{{good.goodName}}</span>
+          </div>
+          <div class="goodInfoKouwei">0.72kg/件,  {{good.kouwei}}</div>
+          <div class="goodInfoPriceAndCount">
+            <div class="goodInfoPrice">￥{{good.price}}</div>
+            <div class="goodCount">x {{good.count}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="peiSong">
+        <span class="peiSongItem1">配送服务</span>
+        <span class="peiSongItem2">京东快递</span>
+      </div>
+      <div class="tuiHuan">
+        <span class="tuiHuanItem1">退换无忧</span>
+        <span class="tuiHuanItem2">￥0.20</span>
+      </div>
+    </div>
+
+    <!-- 卡片 -->
+    <div class="card">
+      <div class="cardItem">
+        <span class="cardItemTitle">促销优惠</span>
+        <div class="cardItemText1">
+          <span>查看更多优惠</span>
+          <img src="../../static/img/backRight.png" alt="">
+        </div>
+      </div>
+      <div class="cardItem">
+        <span class="cardItemTitle">发票信息</span>
+        <div class="cardItemText2">
+          <span>电子发票  个人  商品明细</span>
+          <img src="../../static/img/backRight.png" alt="">
+        </div>
+      </div>
+      <div class="cardItem">
+        <span class="cardItemTitle">礼品卡</span>
+        <div class="cardItemText3">
+          <span>无可用</span>
+          <img src="../../static/img/backRight.png" alt="">
+        </div>
+      </div>
+      <div class="cardItem">
+        <span class="cardItemTitle">京豆</span>
+        <div class="cardItemText3">
+          <span>未满1000个，暂不可使用</span>
+          <img src="../../static/img/backRight.png" alt="">
+        </div>
+      </div>
+    </div>
+
+    <!-- 价格 -->
+    <div class="price">
+
+    </div>
+
 
   </div>
 </template>
@@ -32,6 +99,7 @@ export default {
     return {
       haveAddress: true,
       address: {},
+      goods: [],
       telNum: ''
     }
   },
@@ -53,6 +121,14 @@ export default {
         // 用户之前未保存收货地址
       } else {
 
+      }
+
+      // 获取用户购物车中选中的商品
+      var cart = res.data[0].cart;
+      for (var i = 0; i< cart.length; i++) {
+        if (cart[i].checkStatus === true) {
+          this.goods.push(cart[i]);
+        }
       }
     });
   },
